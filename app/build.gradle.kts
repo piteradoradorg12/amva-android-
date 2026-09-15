42d1fb1355d1753818134eb295613b04474d7987
piteradoradorg12 <piteradoradorg12@gmail.com>
2026-09-14 23:41:25 -0300

    Criar módulo Android do aplicativo AMVA

---
+++ app/build.gradle.kts
@@ -0,0 +1,5 @@
+plugins { id("com.android.application") }
+
+android { namespace = "br.com.amva.associacaomaster"; compileSdk = 35
+    defaultConfig { applicationId = "br.com.amva.associacaomaster"; minSdk = 23; targetSdk = 35; versionCode = 1; versionName = "1.0" }
+}
