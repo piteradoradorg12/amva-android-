5eebba010520572da4bcc9c31bdc185c8d44cc52
piteradoradorg12 <piteradoradorg12@gmail.com>
2026-09-14 23:41:41 -0300

    Criar tela principal do APK AMVA

---
+++ app/src/main/java/br/com/amva/associacaomaster/MainActivity.java
@@ -0,0 +1,42 @@
+package br.com.amva.associacaomaster;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.view.View;
+import android.graphics.Color;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+    private static final String APP_URL = "https://piteradoradorg12.github.io/amva-app/";
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        webView.setBackgroundColor(Color.WHITE);
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setDatabaseEnabled(true);
+        s.setAllowFileAccess(false);
+        s.setAllowContentAccess(false);
+        s.setBuiltInZoomControls(false);
+        s.setDisplayZoomControls(false);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient() {
+            @Override public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return false;
+            }
+        });
+        setContentView(webView);
+        webView.loadUrl(APP_URL);
+    }
+
+    @Override public void onBackPressed() {
+        if (webView.canGoBack()) webView.goBack(); else super.onBackPressed();
+    }
+}
